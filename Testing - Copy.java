5244c25e677af8451850916a93edf2963916b7dd
doddam <34710089+doddam@users.noreply.github.com>
2018-04-16 21:44:14 +0530

    Delete Testing - Copy.java

--- Testing - Copy.java
+++
@@ -1,6 +0,0 @@
-public class Testing {
-    public static void main(String[] args)
-         {
-        System.out.println("Testing whether integration of both github with jenkins done or not...!!!");
-          }
-     }
