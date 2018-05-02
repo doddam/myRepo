48b1d901b2d96c335cc971a13f126939e505d1b2
doddam <34710089+doddam@users.noreply.github.com>
2018-05-02 16:29:35 +0530

    Delete Details.java

--- Details.java
+++
@@ -1,6 +0,0 @@
-public class Details {
-    public static void main(String[] args)
-         {
-        System.out.println("Testing whether integration of both github with jenkins done or not...!!!");
-          }
-     }
